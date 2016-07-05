6ed101412762a137032979f0cf1b7c8096ef2273
saycm01 <callumsay2@gmail.com>
2016-07-05 12:23:43 +0100

    Add files via upload

---
+++ Buttons/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buttons
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            String CallumString = ("You clicked the button!");
+             inputbox = MessageBox.Show("Hello!", CallumString, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (button1 == inputbox.Yes );
+            {
+                MessageBox.Show("You clicked Yes!");
+            }
+            else;
+            {
+                MessageBox.Show("You clicked No!");
+            } 
+            */
+
+            String SecondString = ("Hello World!");
+            MessageBox.Show(SecondString);
+
+
+            
+        }
+    }
+}
